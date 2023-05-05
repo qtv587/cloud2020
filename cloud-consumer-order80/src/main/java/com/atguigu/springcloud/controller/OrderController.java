b3f04943333fe63e6b952ee3b7913aa37fca646d
qtv587 <2512930083@qq.com>
2023-05-05 19:52:30 +0800

    单机版Eureka服务7001

--- cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
@@ -1,7 +1,11 @@
 package com.atguigu.springcloud.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.atguigu.springcloud.entities.CommonResult;
+import com.atguigu.springcloud.entities.Payment;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
 
 /**
  * @Author: XiaoDu
@@ -11,4 +15,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("consumer")
 public class OrderController {
+    public static final String PAYMENT_URL = "http://localhost:8001";
+    @Resource
+    private RestTemplate restTemplate;
+
+
+    @PostMapping("/payment/create")
+    public CommonResult<Payment> create(@RequestBody Payment payment) {
+        return restTemplate.postForObject(PAYMENT_URL + "/payment/create", payment, CommonResult.class);
+    }
+
+    @GetMapping("/payment/get/{id}")
+    public CommonResult<Payment> selectOne(@PathVariable("id") Long id){
+//        System.out.println(PAYMENT_URL+"/payment/get/"+id);
+        return restTemplate.getForObject(PAYMENT_URL+"/payment/get/"+id,CommonResult.class);
+    }
 }
