26d79f7ef3581fac4add6a6de3bc7c55c7356413
xiaodu <2512930083@qq.com>
2023-12-19 17:09:12 +0800

    openFeign使用

---
+++ cloud-consumer-feign-order-80/src/main/java/com/atguigu/springcloud/OrderFeignMain80.java
@@ -0,0 +1,20 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignClient;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+@EnableFeignClients
+public class OrderFeignMain80
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(OrderFeignMain80.class,args);
+    }
+}
