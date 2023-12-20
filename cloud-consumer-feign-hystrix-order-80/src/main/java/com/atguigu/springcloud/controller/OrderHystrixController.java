1ad0cd3ae61872dd8c7844a153413b683e9a5b4f
xiaodu <2512930083@qq.com>
2023-12-20 13:59:13 +0800

    Hystrix消费端服务降级

--- cloud-consumer-feign-hystrix-order-80/src/main/java/com/atguigu/springcloud/controller/OrderHystrixController.java
+++ cloud-consumer-feign-hystrix-order-80/src/main/java/com/atguigu/springcloud/controller/OrderHystrixController.java
@@ -2,6 +2,9 @@ package com.atguigu.springcloud.controller;
 
 
 import com.atguigu.springcloud.service.PaymentHystrixService;
+import com.netflix.hystrix.contrib.javanica.annotation.DefaultProperties;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,8 +28,17 @@ public class OrderHystrixController {
     }
 
     @GetMapping("/hystrix/timeout/{id}")
+    @HystrixCommand(fallbackMethod = "paymentTimeOutFallbackMethod",commandProperties = {
+            @HystrixProperty(name="execution.isolation.thread.timeoutInMilliseconds",value="1500")
+    })
         public String paymentInfoTimeOut(@PathVariable("id") Integer id){
             String result = paymentHystrixService.paymentInfoTimeOut(id);
             return result;
         }
+
+    public String paymentTimeOutFallbackMethod(@PathVariable("id") Integer id)
+    {
+        return "我是消费者80,对方支付系统繁忙请10秒钟后再试或者自己运行出错请检查自己,o(╥﹏╥)o";
+    }
+
 }
--- cloud-provider-hystrix-payment-8001/src/main/java/com/atguigu/springcloud/service/PaymentServiceImpl.java
+++ cloud-provider-hystrix-payment-8001/src/main/java/com/atguigu/springcloud/service/PaymentServiceImpl.java
@@ -1,5 +1,7 @@
 package com.atguigu.springcloud.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.TimeUnit;
@@ -12,13 +14,17 @@ public class PaymentServiceImpl implements PaymentService {
     }
 
     @Override
+    @HystrixCommand(fallbackMethod = "paymentInfoTimeOutHandler",commandProperties = {@HystrixProperty(name="execution.isolation.thread.timeoutInMilliseconds",value="5000")})
     public String paymentInfoTimeOut(Integer id) {
-        Integer timeOutNumber = 5;
+        Integer timeOutNumber = 3;
         try {
             TimeUnit.SECONDS.sleep(timeOutNumber);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
         return "线程池： " + Thread.currentThread().getName() +"\t"+ "paymentInfoTimeout(),id:"+ id+ "\t" +" \"O(∩_∩) 成功返回哈哈哈";
+    }
+    public String paymentInfoTimeOutHandler(Integer id){
+        return "线程池： " + Thread.currentThread().getName() +"\t"+ "paymentInfoTimeOutHandler(),id:"+ id+ "\t" +" \"/(ㄒoㄒ)/~~ ";
     }
 }
