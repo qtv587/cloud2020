c7d1b0e3fca8b031165dfd0795764ec60887d7d8
xiaodu <2512930083@qq.com>
2023-12-23 00:00:37 +0800

    Sentinel 服务熔断搭配openfeign和rabbin

---
+++ cloudalibaba-provider-payment-9004/src/main/java/com/atguigu/springcloud/PaymentApplication9004.java
@@ -0,0 +1,17 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PaymentApplication9004 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentApplication9004.class, args);
+    }
+}
