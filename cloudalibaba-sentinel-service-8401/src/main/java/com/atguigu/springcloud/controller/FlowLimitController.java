fc22e20a0a4a247638a67fae1fb8d034be67d885
xiaodu <2512930083@qq.com>
2023-12-22 18:58:45 +0800

    Sentinel 热点key,参数例外项，不包含异常

--- cloudalibaba-sentinel-service-8401/src/main/java/com/atguigu/springcloud/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service-8401/src/main/java/com/atguigu/springcloud/controller/FlowLimitController.java
@@ -1,9 +1,14 @@
 package com.atguigu.springcloud.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class FlowLimitController {
 
     @GetMapping("testA")
@@ -19,4 +24,16 @@ public class FlowLimitController {
     public String testD(){
         return "-----testD";
     }
+    @GetMapping("/testHotkey")
+    @SentinelResource(value = "testHotkey",blockHandler = "deal_testHotkey")
+    public String testHotkey(@RequestParam(value = "p1",required = false) String p1,
+                             @RequestParam(value = "p2",required = false) String p2){
+        return "-----testHotkey";
+    }
+
+    public String deal_testHotkey(String p1, String p2, BlockException exception){
+        log.info(p1+"-------"+p2);
+        return "-------deal_testHotkey,o(╥﹏╥)o";
+    }
+
 }
